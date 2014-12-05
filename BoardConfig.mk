03dfd3508226b5429e98f78b7b86b0393bdb14c0
martin coulon <martinusbe@gmail.com>
2014-12-05 19:32:02 +0100

    VALIDUS4LIFE=true

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -24,7 +24,8 @@ TARGET_OTA_ASSERT_DEVICE := xt1031,xt1032,xt1033,xt1034,falcon_umts,falcon_umtsd
 # Kernel
 BOARD_CUSTOM_BOOTIMG_MK := $(LOCAL_PATH)/mkbootimg.mk
 TARGET_KERNEL_CONFIG := falcon_defconfig
-TARGET_GCC_VERSION_ARM := arm-eabi-4.9-sm
+TARGET_GCC_VERSION_ARM := arm-eabi-4.9
+VALIDUS4LIFE=true
 
 # Init
 TARGET_INIT_VENDOR_LIB := libinit_msm
